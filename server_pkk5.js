b24364c8bb3d89d16c2afe5de0a95c1c26be8efa
thishome153 (at Office) <home153@mail.ru>
2019-07-30 17:42:27 +0300

    -

--- server_pkk5.js
+++ server_pkk5.js
@@ -1,5 +1,6 @@
 
 var packageJSON = require('./package.json');
+var https = require('https');
 
 exports.GETReq = function (req, res) {
 	console.log('detected request GET...pkk5 wrapper');
@@ -8,15 +9,37 @@ exports.GETReq = function (req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.setHeader('Content-Type', 'application/json');
 
-	res.send(JSON.stringify({
-		"ServiceName": packageJSON.name,
-		"Description": packageJSON.description,
-		"Version": packageJSON.version,
-		"Client": clientip + " login log",
-        "Request": req.query,
-        "Purpose" : "pkk5 REST api wrapper",
-		"Timestamp": tm,
-		"state": 200,
-		"stateText": "Server ok"
-	}));
+	https.get('https://pkk5.rosreestr.ru/api/features/1/26:5:43019:22', (resp) => {
+		let data = '';
+
+		// A chunk of data has been recieved.
+		resp.on('data', (chunk) => {
+			data += chunk;
+		});
+		// The whole response has been received. Print out the result.
+		resp.on('end', () => {
+			console.log(JSON.parse(data).explanation);
+		});
+
+
+		res.send(JSON.stringify({
+			"ServiceName": packageJSON.name,
+			"Description": packageJSON.description,
+			"Version": packageJSON.version,
+			"Client": clientip + " login log",
+			"Request": req.query,
+			"Response" : resp,
+			"Purpose": "pkk5 REST api wrapper",
+			"Timestamp": tm,
+			"state": 200,
+			"stateText": "Server ok"
+		}));
+
+
+
+	}).on("error", (err) => {
+		console.log("Error: " + err.message);
+	});
+
+
 };
